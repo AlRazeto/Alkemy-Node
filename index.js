e30c0d73d2883585a0aad0cecfd19464fe5e8b9a
alejo <alejorazeto@gmail.com>
2022-02-05 21:00:35 -0300

    .env aplicado apropiadamente y error middleware

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const sequelize = require('./libs/sequelize');
 const routerAPI = require('./routes/index');
+const {errorHandler, logError} = require('./middlewares/error.middleware');
 
 const app = express();
 const port = 8080;
 
 app.get('/', (req, res)=>{
     console.log('listening')
-})
+});
+
+//routing
+
+app.use(logError);
+app.use(errorHandler);
 
 app.listen(port, async()=>{
     try {
@@ -16,4 +22,4 @@ app.listen(port, async()=>{
       } catch (error) {
         console.error('Unable to connect to the database:', error);
       }
-})
+});
